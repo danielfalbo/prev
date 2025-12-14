ba0e05c8fd98741fbf969ed115805b82692ed063
Daniel <39460524+danielfalbo@users.noreply.github.com>
2025-12-14 17:06:08 +0100

    make edit.sh compile all and open edited before exit

--- edit.sh
+++ edit.sh
@@ -12,3 +12,9 @@ sqlite3 knowledge.db "SELECT html FROM \"$1\" WHERE slug='$2';" > buf.html
 vi -c "set backupcopy=yes" buf.html
 
 sqlite3 knowledge.db "UPDATE \"$1\" SET html=CAST(readfile('buf.html') as TEXT) WHERE slug='$2';"
+
+python3 main.py
+
+if [[ "$(uname -s)" == "Darwin" ]]; then
+    open "dist/$1/$2.html"
+fi
